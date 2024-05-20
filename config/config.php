ce60891b918901ef0f9c1e7dd4cc242cb031ad40
cassiopeelaurie <cassiopee.laurie@gmail.com>
2024-05-20 10:03:15 +0200

    added recherche.php and modified index

---
+++ config/config.php
@@ -0,0 +1,6 @@
+<?php
+define('DB_HOST', 'localhost');  // Nom de l'hôte
+define('DB_NAME', 'nom_de_votre_base_de_donnees');  // Nom de la base de données
+define('DB_USER', 'votre_nom_utilisateur');  // Nom d'utilisateur
+define('DB_PASS', 'votre_mot_de_passe');  // Mot de passe
+?>
